76142194e7784f38961868d69c7ce09919d04842
FerminAMC <a01209665@itesm.mx>
2018-11-19 15:38:24 -0600

    Dataset downloaded from spotify. Started data analysis with pandas and matplotlib

--- spotify.py
+++ spotify.py
@@ -1,4 +1,6 @@
 import pprint
+import random
+import csv
 import os
 import sys
 import json
@@ -8,29 +10,78 @@ import spotipy.util as util
 from json.decoder import JSONDecodeError
 from env import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI
 
-username = 'ferminamc'
-scope = 'user-library-read'
-client_id = SPOTIPY_CLIENT_ID
-client_secret = SPOTIPY_CLIENT_SECRET
-redirect_uri = SPOTIPY_REDIRECT_URI
+import numpy as np
+import pandas
+from sklearn.neural_network import MLPClassifier
 
-# Get access token and delete cache in case it fails
-try:
-    token = util.prompt_for_user_token(username,scope,client_id=client_id,client_secret=client_secret,redirect_uri=redirect_uri)
-except:
-    os.remove(f".cache-{username}")
-    token = util.prompt_for_user_token(username,scope,client_id=client_id,client_secret=client_secret,redirect_uri=redirect_uri)
+# Creates a CSV file with songs I like and dislike for later use
+def create_csv():
+    username = 'ferminamc'
+    scope = 'user-library-read'
+    client_id = SPOTIPY_CLIENT_ID
+    client_secret = SPOTIPY_CLIENT_SECRET
+    redirect_uri = SPOTIPY_REDIRECT_URI
 
-# Create a Spotify object
-col_names = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'loudness', 'speechiness']
-if token:
-    sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks()
-    for item in results['items']:
-        track = item['track']
-        track_id = track['id']
-        pprint.pprint(sp.audio_features(track_id)[0]['acousticness'])
-        break
-        #print(track['name'] + ' - ' + track['artists'][0]['name'])
-else:
-    print("Can't get token for", username)
+    # Get access token and delete cache in case it fails
+    try:
+        token = util.prompt_for_user_token(username,scope,client_id=client_id,client_secret=client_secret,redirect_uri=redirect_uri)
+    except:
+        os.remove(f".cache-{username}")
+        token = util.prompt_for_user_token(username,scope,client_id=client_id,client_secret=client_secret,redirect_uri=redirect_uri)
+
+    col_names = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'loudness', 'speechiness', 'liked']
+    train_data = []
+    train_data.append(col_names)
+    repeat = 3
+    limit = 20 # Number of songs that will be downloaded
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        # Get 100 liked songs
+        for i in range(repeat):
+            results = sp.current_user_saved_tracks(offset=(i*limit))
+            for item in results['items']:
+                temp_data = []
+                track_id = item['track']['id']
+                track_features = sp.audio_features(track_id)
+                for col in col_names[0:-1]:
+                    temp_data.append(track_features[0][col])
+                temp_data.append(1)
+                train_data.append(temp_data)
+
+        # K-pop playlist
+        k_pop_user = 'spotify'
+        k_pop_playlist = '37i9dQZF1DX9tPFwDMOaN1?si=EIk4w9gVTImLOHtRfvqY8g'
+        # Getting songs I don't like for the training
+        results = sp.user_playlist(k_pop_user, k_pop_playlist)
+        for item in results['tracks']['items']:
+            temp_data = []
+            track_id = item['track']['id']
+            track_features = sp.audio_features(track_id)
+            for col in col_names[0:-1]:
+                temp_data.append(track_features[0][col])
+            temp_data.append(0)
+            train_data.append(temp_data)
+        # Global Top 50
+        top_50_user = 'spotifycharts'
+        top_50_playlist = '37i9dQZEVXbMDoHDwVN2tF'
+        results = sp.user_playlist(top_50_user, top_50_playlist)
+        for item in results['tracks']['items']:
+            temp_data = []
+            track_id = item['track']['id']
+            track_features = sp.audio_features(track_id)
+            for col in col_names[0:-1]:
+                temp_data.append(track_features[0][col])
+            temp_data.append(random.uniform(0.4, 0.6))
+            train_data.append(temp_data)
+    else:
+        print("Can't get token for", username)
+
+    pprint.pprint(train_data)
+
+    with open('train_data.csv', 'w', newline='') as csvfile:
+        write_csv = csv.writer(csvfile)
+        write_csv.writerow(train_data[0])
+        for row in train_data[1:]:
+            write_csv.writerow(row)
+
+#create_csv()
